0be357cda9906a6cfb5bb988c73e6d136374cda6
Willy e-Sharp <willy.esharp@gmail.com"D:\Willy\Desktop\cpp_general_recursive_functions\build\test.exe>
2020-11-07 17:40:33 +0100

    fixed examples

--- test.cpp
+++ test.cpp
@@ -1,3 +1,4 @@
+#include <recursive/general.hpp>
 #include <recursive/all_primitive.hpp>
 
 #include <iostream>
@@ -6,8 +7,10 @@ using namespace rec;
 
 auto P = Rec(O<0>, Pi<1, 2>);
 
-auto Plus = Rec(Pi<1, 2>, Comp(S, Pi<3, 3>));
+auto Sum = Rec(Pi<1, 1>, Comp(S, Pi<3, 3>));
+// Truncated difference.
+auto TDiff = Comp(Rec(Pi<1, 1>, Comp(P, Pi<3, 3>)), Pi<2, 2>, Pi<1, 2>);
 
 int main() {
-    std::cout << eval(P, 10u) << std::endl;
+    std::cout << eval(Diff, 17u, 13u) << std::endl;
 }
