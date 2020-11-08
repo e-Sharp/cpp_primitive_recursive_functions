f9ea9d8b95b8cfaea900e814fa2b9847d867d919
Willy e-Sharp <willy.esharp@gmail.com>
2020-11-08 17:00:08 +0100

    Temporary changes.

--- test.cpp
+++ test.cpp
@@ -1,16 +1,14 @@
-#include <recursive/all_general.hpp>
+#include <recursive/example/all.hpp>
+// #include <recursive/primitive.hpp>
 
 #include <iostream>
 
 using namespace rec;
 
-constexpr auto P = Rec(O<0>, Pi<1, 2>);
-
-constexpr auto Sum = Rec(Pi<1, 1>, Comp(S, Pi<3, 3>));
-// Truncated difference.
-constexpr auto TDiff = Comp(Rec(Pi<1, 1>, Comp(P, Pi<3, 3>)), Pi<2, 2>, Pi<1, 2>);
-
 int main() {
-    constexpr auto Id = Min(Rec(Pi<1, 1>, Comp(P, Pi<3, 3>)));
-    std::cout << eval(Id, 17u) << std::endl;
+    std::cout << eval(Xor, 0u, 0u) << std::endl;
+    std::cout << eval(Xor, 0u, 1u) << std::endl;
+    std::cout << eval(Xor, 1u, 0u) << std::endl;
+    std::cout << eval(Xor, 1u, 1u) << std::endl;
+    
 }
